892c65ea435d18fd38c531416e06302e6d5075de
Muhammad Hassaan <mhassaan3786@gmail.com>
2025-07-15 02:04:49 +0500

    Update config.js
    I have change the base url

--- config.js
+++ config.js
@@ -1,1 +1,1 @@
-export const API_BASE = "http://localhost:3000"; //my url for backend
+export const API_BASE = "https://c156509e-caba-4e4e-a323-00637eb85bed-00-3akrleo9hkw7g.pike.replit.dev/"; //my url for backend
